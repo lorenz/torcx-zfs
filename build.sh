d203ed8d28ed1083506d5f4dc37a846395459e9c
Lorenz Brun <lorenz@dolansoft.org>
2018-01-11 19:10:52 +0100

    Added Systemd and proper wrappers and cleanup of final module

--- build.sh
+++ build.sh
@@ -19,26 +19,41 @@ make -j$(nproc)
 make install
 
 cd $BUILDDIR/zfs-$ZOL_VERSION
-./autogen.sh 
+./autogen.sh
 ./configure --prefix=$BUILDDIR/root --with-linux=/usr/lib64/modules/$(ls /lib64/modules)/source --with-linux-obj=/usr/lib64/modules/$(ls /lib64/modules)/build --with-spl=$BUILDDIR/spl-spl-$ZOL_VERSION/
 automake
 make -j$(nproc)
 make install
 
 cd $BUILDDIR
+mkdir -p root/lib/modules
+cp -a /lib64/modules/$(ls /lib64/modules)/extra/. root/lib/modules/
+
+mkdir -p root/lib/systemd/system
+cp -a *.service root/lib/systemd/system/
+cp -a *.target root/lib/systemd/system/
+mkdir -p root/lib/systemd/system/zfs.target.wants
+ln -s ../zfs-mount.service root/lib/systemd/system/zfs.target.wants/zfs-mount.service
+ln -s ../zfs-import-scan.service root/lib/systemd/system/zfs.target.wants/zfs-import-scan.service
+ln -s ../zfs-import-cache.service root/lib/systemd/system/zfs.target.wants/zfs-import-cache.service
+mkdir -p root/lib/systemd/system/multi-user.target.wants
+ln -s ../zfs.target root/lib/systemd/system/zfs.target.wants/zfs.target
+
+WRAP="fsck.zfs zdb zed zfs zpios zpool zstreamdump zvol_id"
+
+mkdir -p root/wrap
+for command in "$WRAP"; do
+    cp wrapper.sh root/wrap/$command
+    sed -i "s|COMMAND|$command|g" root/wrap/$command
+done
+
 mkdir -p root/.torcx
 cp manifest.json root/.torcx/manifest.json
 
+rm -Rf root/include
+rm -Rf root/*.a
+rm -Rf root/src
+rm -Rf root/share
+
 cd root
 tar --force-local -zcf ../zfs:$ZOL_VERSION.torcx.tgz .
-
-#WRAP="fsck.zfs mount.zfs zdb zfs zhack zinject zpios zpool zstreamdump ztest vdev_id zvol_id"
-
-#mkdir -p /target/bin
-#mkdir -p /target/wrap
-#for command in "$WRAP"; do
-#    LOC=$(which $command)
-#    cp wrapper.sh /target/wrap/$command
-#    sed -i "s|COMMAND|$command|g" /target/wrap/$command
-#    cp $LOC /target/bin/
-#done
---
+++ wrapper.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+source /run/metadata/torcx
+export LD_LIBRARY_PATH=$TORCX_UNPACKDIR/zfs/lib:$TORCX_UNPACKDIR/zfs/lib64
+export PATH=$PATH:$TORCX_UNPACKDIR/zfs/sbin:$TORCX_UNPACKDIR/zfs/bin
+exec $TORCX_UNPACKDIR/zfs/sbin/COMMAND

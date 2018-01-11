cb2619e02d54af92a3ad359f11c646ba280e2c51
Lorenz Brun <lorenz@dolansoft.org>
2018-01-11 18:12:24 +0100

    First test build (do not use)

---
+++ build.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+set -ev
+
+BUILDDIR=$(pwd)
+mkdir root
+
+emerge-gitclone
+
+wget -q -O - https://github.com/zfsonlinux/zfs/releases/download/zfs-$ZOL_VERSION/zfs-$ZOL_VERSION.tar.gz | tar -xzf -
+wget -q -O - https://github.com/zfsonlinux/spl/archive/spl-$ZOL_VERSION.tar.gz | tar -xzf -
+
+emerge automake autoconf libtool
+
+cd $BUILDDIR/spl-spl-$ZOL_VERSION
+./autogen.sh
+./configure --prefix=$BUILDDIR/root  --with-linux=/usr/lib64/modules/$(ls /lib64/modules)/source --with-linux-obj=/usr/lib64/modules/$(ls /lib64/modules)/build
+automake
+make -j$(nproc)
+make install
+
+cd $BUILDDIR/zfs-$ZOL_VERSION
+./autogen.sh 
+./configure --with-linux=/usr/lib64/modules/$(ls /lib64/modules)/source --with-linux-obj=/usr/lib64/modules/$(ls /lib64/modules)/build --with-spl=/root/spl-spl-$ZOL_VERSION/
+automake
+make -j$(nproc)
+make install
+
+cd $BUILDDIR
+mkdir -p root/.torcx
+cp manifest.json root/.torcx/manifest.json
+
+cd root
+tar --force-local -zcf ../zfs:$ZOL_VERSION.torcx.tgz .
+
+#WRAP="fsck.zfs mount.zfs zdb zfs zhack zinject zpios zpool zstreamdump ztest vdev_id zvol_id"
+
+#mkdir -p /target/bin
+#mkdir -p /target/wrap
+#for command in "$WRAP"; do
+#    LOC=$(which $command)
+#    cp wrapper.sh /target/wrap/$command
+#    sed -i "s|COMMAND|$command|g" /target/wrap/$command
+#    cp $LOC /target/bin/
+#done

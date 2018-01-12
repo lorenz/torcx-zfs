76891e76bec369e1f40dccdd76667a61b43a57e9
Lorenz Brun <lorenz@dolansoft.org>
2018-01-12 19:40:12 +0100

    Acquire root in wrapper

--- wrapper.sh
+++ wrapper.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+if [ "$(id -u)" != "0" ]; then
+  exec sudo "$0" "$@" 
+fi
+
 source /run/metadata/torcx
 export LD_LIBRARY_PATH=$TORCX_UNPACKDIR/zfs/lib:$TORCX_UNPACKDIR/zfs/lib64
 export PATH=$PATH:$TORCX_UNPACKDIR/zfs/sbin:$TORCX_UNPACKDIR/zfs/bin

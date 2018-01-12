693d374aef8373f063149618badae99c5c4c4f14
Lorenz Brun <lorenz@dolansoft.org>
2018-01-12 01:18:47 +0100

    Fix wrapper

--- wrapper.sh
+++ wrapper.sh
@@ -2,4 +2,4 @@
 source /run/metadata/torcx
 export LD_LIBRARY_PATH=$TORCX_UNPACKDIR/zfs/lib:$TORCX_UNPACKDIR/zfs/lib64
 export PATH=$PATH:$TORCX_UNPACKDIR/zfs/sbin:$TORCX_UNPACKDIR/zfs/bin
-exec $TORCX_UNPACKDIR/zfs/sbin/COMMAND
+exec $TORCX_UNPACKDIR/zfs/sbin/COMMAND "$@"

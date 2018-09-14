d04fdc563800cc0f3c14cbcd45778373ab68c0dc
Lorenz Brun <lorenz@dolansoft.org>
2018-09-14 22:06:41 +0000

    Unconditionally reenable Perl patch

--- build.sh
+++ build.sh
@@ -7,10 +7,9 @@ mkdir udev
 
 emerge-gitclone
 
-if ! perl -e 'print "Perl works\n"'; then
-  patch $(equery w dev-lang/perl) perl-fwrapv-fix.patch
-  emerge --nodep -e dev-lang/perl # VERY BAD, but this environment is going to get destroyed in a few minutes so whatever
-fi
+patch $(equery w dev-lang/perl) perl-fwrapv-fix.patch
+emerge --nodep -e dev-lang/perl # VERY BAD, but this environment is going to get destroyed in a few minutes so whatever
+
 
 wget -q -O - https://github.com/zfsonlinux/zfs/releases/download/zfs-$ZOL_VERSION/zfs-$ZOL_VERSION.tar.gz | tar -xzf -
 wget -q -O - https://github.com/zfsonlinux/spl/archive/spl-$ZOL_VERSION.tar.gz | tar -xzf -

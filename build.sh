0d8c20994c742b9d7d145401645012468efbd742
Lorenz Brun <lorenz@dolansoft.org>
2019-02-27 23:25:55 +0000

    Remove attempted hack and upgrade to 0.7.12 for 4.19+

--- build.sh
+++ build.sh
@@ -24,7 +24,6 @@ make -j$(nproc)
 make install
 
 cd $BUILDDIR/zfs-$ZOL_VERSION
-sed -i 's/-Wall//' config/Rules.am
 ./autogen.sh
 ./configure --prefix=$BUILDDIR/root --with-linux=/usr/lib64/modules/$(ls /lib64/modules)/source --with-linux-obj=/usr/lib64/modules/$(ls /lib64/modules)/build --with-spl=$BUILDDIR/spl-spl-$ZOL_VERSION/ --with-udevdir=$BUILDDIR/udev
 automake
